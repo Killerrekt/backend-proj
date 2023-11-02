8546978c642465c3d442e829182e083b4dcf4901
Aman L <amankhanter14@gmail.com>
2023-11-02 10:03:11 +0530

    Added authorization to cors header

--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     config.ClientOrigin,
-		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowHeaders:     "Origin, Content-Type, Accept , Authorization",
 		AllowMethods:     "GET, POST, PATCH, DELETE",
 		AllowCredentials: true,
 	}))
